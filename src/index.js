43d2b086b20e09435bc1ae9aca341e1ffe2d8e35
igdkaur <igdkaur@gmail.com>
2021-11-29 16:22:59 +0530

    base code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const element = (<div> Hey </div>)
+  
+function Tweet() {
+  //1. update the Tweet component to accept a tweet prop
+  return (
+    <div className="tweet">
+      HEY
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Tweet  />, document.querySelector("#root"));
