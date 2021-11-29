30aa6bf5d3c611097fe86c4403e5229670662607
igdkaur <igdkaur@gmail.com>
2021-11-29 17:16:53 +0530

    Display files names

--- src/index.js
+++ src/index.js
@@ -1,16 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import PropTypes from 'prop-types';
 
-const element = (<div> Hey </div>)
-  
-function Tweet() {
-  //1. update the Tweet component to accept a tweet prop
-  return (
-    <div className="tweet">
-      HEY
-    </div>
-  );
-}
+// Create the FileList component. In the interest of doing the simplest thing that
+// can possibly work, we’ll render a plain unordered list of file names. Once it
+// works we’ll extract the list items into a FileListItem component.
+const testFiles = [
+  {
+    id: 1,
+    name: 'src',
+    type: 'folder',
+    updated_at: "2016-07-11 21:24:00",
+    latestCommit: {
+      message: 'Initial commit'
+    }
+  },
+  {
+    id: 2,
+    name: 'tests',
+    type: 'folder',
+    updated_at: "2016-07-11 21:24:00",
+    latestCommit: {
+      message: 'Initial commit'
+    }
+  },
+  {
+    id: 3,
+    name: 'README',
+    type: 'file',
+    updated_at: "2016-07-18 21:24:00",
+    latestCommit: {
+      message: 'Added a readme'
+    }
+  },
+];
 
-ReactDOM.render(<Tweet  />, document.querySelector("#root"));
+const FileList = ({ files }) => (
+  <table className="file-list">
+    <tbody>
+      {files.map((file) => (
+        <tr className="file-list-item" key={file.id}>
+          <td className="file-name">{file.name}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+FileList.propTypes = { // notice small p
+  files:PropTypes.array // notice big P
+};
+
+ReactDOM.render(<FileList files={testFiles}  />, document.querySelector("#root"));
